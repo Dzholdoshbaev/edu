460c1553e5bb7d0c0a63b7f296e8e6a34eaf4389
abdyrakhman <medzholdoshbaev@gmail.com>
2024-09-04 20:12:44 +0600

    create service for restaurants

---
+++ src/main/java/dzholdoshbaev/laboratory/service/RestaurantsService.java
@@ -0,0 +1,9 @@
+package dzholdoshbaev.laboratory.service;
+
+import dzholdoshbaev.laboratory.model.Restaurants;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface RestaurantsService {
+    Page<Restaurants> getAllRestaurants(Pageable pageable);
+}
---
+++ src/main/java/dzholdoshbaev/laboratory/service/impl/RestaurantsServiceImpl.java
@@ -0,0 +1,27 @@
+package dzholdoshbaev.laboratory.service.impl;
+
+import dzholdoshbaev.laboratory.model.Restaurants;
+import dzholdoshbaev.laboratory.repository.RestaurantsRepository;
+import dzholdoshbaev.laboratory.service.RestaurantsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class RestaurantsServiceImpl implements RestaurantsService {
+    private final RestaurantsRepository restaurantsRepository;
+
+    @Override
+    public Page<Restaurants> getAllRestaurants(Pageable pageable) {
+        Page<Restaurants> restaurantsPage = restaurantsRepository.findAll(pageable);
+        List<Restaurants> restaurants = restaurantsPage.getContent();
+        return new PageImpl<>(restaurants, pageable, restaurantsPage.getTotalElements());
+    }
+}
