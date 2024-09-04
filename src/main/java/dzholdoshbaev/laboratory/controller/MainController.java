a21d60e70a3b7c21aa0978519bf83b544c7e472c
abdyrakhman <medzholdoshbaev@gmail.com>
2024-09-04 20:13:15 +0600

    create controller and ftlh file for main url

--- src/main/java/dzholdoshbaev/laboratory/controller/MainController.java
+++ src/main/java/dzholdoshbaev/laboratory/controller/MainController.java
@@ -1,6 +1,10 @@
 package dzholdoshbaev.laboratory.controller;
 
+import dzholdoshbaev.laboratory.service.RestaurantsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class MainController {
+    private final RestaurantsService restaurantsService;
+
     @GetMapping
-    public String main(Model model) {
+    public String getAllRestaurants(Model model ,  @PageableDefault(size = 5, sort = "name") Pageable pageable) {
+        model.addAttribute("restaurants",restaurantsService.getAllRestaurants(pageable));
         return "main";
     }
 }
